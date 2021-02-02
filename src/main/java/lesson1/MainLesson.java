3c322e3cc5ce971f01090b5c96468390c9fa8d3e
MTO studio <info@mtostudio.com>
2021-02-02 14:54:37 +0500

    Пытаюсь настроить выгрузку на GitHub

--- src/main/java/lesson1/MainLesson.java
+++ src/main/java/lesson1/MainLesson.java
@@ -2,12 +2,6 @@ package lesson1;
 
 public class MainLesson {
     public static void main(String[] args) {
-        //System.out.println("Hello world");
-        testVars();
-    }
 
-    public static void testVars() {
-        int varA = 3;
-        System.out.println(varA);
     }
 }
