18a84d16160b4a8e254efd569c9dc6b7f7fc68a1
MTO studio <info@mtostudio.com>
2021-02-10 12:50:13 +0500

    Сделал тест скорости работы разных вариантов проверки длины массива для Зелимхана

---
+++ src/main/java/perf/test/MainPerfTest.java
@@ -0,0 +1,117 @@
+package perf.test;
+
+import java.util.Arrays;
+
+public class MainPerfTest {
+    public static void main(String[] args) {
+        String msg;
+        // Запускаем каждый вариант по 10 раз, считаем среднее арифметическое
+        double[] execTimes3 = new double[10];
+        for (int i = 0; i < 10; i++) {
+            execTimes3[i] = perfTestWithConst();
+        }
+        msg = "Вариант с константой отработывает за: %.2f мс.%n";
+        System.out.printf(msg, calcArrayAverage(execTimes3));
+
+        double[] execTimes2 = new double[10];
+        for (int i = 0; i < 10; i++) {
+            execTimes2[i] = perfTestWithVar();
+        }
+        msg = "Вариант с переменной отработывает за: %.2f мс.%n";
+        System.out.printf(msg, calcArrayAverage(execTimes2));
+
+        double[] execTimes = new double[10];
+        for (int i = 0; i < 10; i++) {
+            execTimes[i] = perfTestWithoutVar();
+        }
+        msg = "Вариант с arr1.length отработывает за: %.2f мс.%n";
+        System.out.printf(msg, calcArrayAverage(execTimes));
+    }
+
+    //Посчитать среднее арифметическое для массива double
+    public static double calcArrayAverage(double[] arr1) {
+        double sum = 0;
+        for (double val : arr1) {
+            sum += val;
+        }
+        return sum / arr1.length;
+    }
+
+    // Создать массив и заполнить его случайными числами от 0 до 100
+    public static int[] genArray() {
+        final int SIZE = 30000;
+        int[] arr1 = new int[SIZE];
+        for (int i = 0; i < arr1.length; i++) {
+            arr1[i] = (int) (Math.random() * 100);
+        }
+        return arr1;
+    }
+
+    // Без переменной
+    private static double perfTestWithoutVar() {
+        int[] arr1 = genArray();
+
+        // Засекаем время
+        long m = System.currentTimeMillis();
+
+        // Сортируем массив
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = i + 1; j < arr1.length; j++) {
+                if (arr1[i] < arr1[j]) {
+                    int tmp = arr1[i];
+                    arr1[i] = arr1[j];
+                    arr1[j] = tmp;
+                }
+            }
+        }
+
+        // Возвращаем итоговое время выполнения
+        return System.currentTimeMillis() - m;
+    }
+
+    // С переменной
+    private static double perfTestWithVar() {
+        int[] arr1 = genArray();
+
+        // Засекаем время
+        long m = System.currentTimeMillis();
+
+        // Сортируем массив
+        int len = arr1.length;
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len; j++) {
+                if (arr1[i] < arr1[j]) {
+                    int tmp = arr1[i];
+                    arr1[i] = arr1[j];
+                    arr1[j] = tmp;
+                }
+            }
+        }
+
+        // Возвращаем итоговое время выполнения
+        return System.currentTimeMillis() - m;
+    }
+
+    // С константой
+    private static double perfTestWithConst() {
+        int[] arr1 = genArray();
+
+        // Засекаем время
+        long m = System.currentTimeMillis();
+
+        // Сортируем массив
+        final int len = arr1.length;
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len; j++) {
+                if (arr1[i] < arr1[j]) {
+                    int tmp = arr1[i];
+                    arr1[i] = arr1[j];
+                    arr1[j] = tmp;
+                }
+            }
+        }
+
+        // Возвращаем итоговое время выполнения
+        return System.currentTimeMillis() - m;
+    }
+}
